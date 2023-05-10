0f47bbad371396a38a5c6b0554acb76dfaf365b3
RatserX <12604047+RatserX@users.noreply.github.com>
2023-05-09 20:38:28 -0500

    perf: update version

---
+++ dist/sdqe.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name Steam Discovery Queue Explorer
+// @description Alternate explorer for the Steam Discovery Queue
+// @version 6.0.0
+// @author RatserX
+// @homepage https://github.com/RatserX/steam-discovery-queue-explorer#readme
+// @supportURL https://github.com/RatserX/steam-discovery-queue-explorer/issues
+// @match http://store.steampowered.com/explore*
+// @match https://store.steampowered.com/explore*
+// @downloadURL https://github.com/RatserX/steam-discovery-queue-explorer/raw/main/dist/sdqe.user.js
+// @icon https://store.steampowered.com/favicon.ico
+// @namespace https://github.com/RatserX/steam-discovery-queue-explorer
+// @updateURL https://github.com/RatserX/steam-discovery-queue-explorer/raw/main/dist/sdqe.meta.js
+// ==/UserScript==
